b34464b5ff5e419ade47675aa6672af7a6c34865
SSep <ssep4u@gmail.com>
2020-09-18 00:43:06 +0900

    urls, cart/urls detail, product_add, product_remove

--- DGoodsShop/urls.py
+++ DGoodsShop/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
+    path('', include('shop.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
